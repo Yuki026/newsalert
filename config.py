950a0f862008918e8da60c194fb1a195e2768202
Qst236 <167086442+Qst236@users.noreply.github.com>
2024-10-18 18:18:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,3 +22,19 @@ MONTH = {
     'Nov' : 'November',
     'Dec' : 'Desember'
 }
+
+ALLOWED_ELEMENT_TYPES = {
+    "calendar__cell": "date",
+    "calendar__cell calendar__date": "date",
+    "calendar__cell calendar__time": "time",
+    "calendar__cell calendar__currency": "currency",
+    "calendar__cell calendar__impact": "impact",
+    "calendar__cell calendar__event event": "event"
+}
+
+ICON_COLOR_MAP = {
+    "icon icon--ff-impact-yel": "yellow",
+    "icon icon--ff-impact-ora": "orange",
+    "icon icon--ff-impact-red": "red",
+    "icon icon--ff-impact-gra": "gray"
+}
